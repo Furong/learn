7b23bb6acf5e59ae2ad8f8d1cc05bac5bece13c4
Furong <dingfurong@hikvision.com.cn>
2019-07-19 18:48:33 +0800

    add http related codes

---
+++ java/doc/HTTP.md
@@ -0,0 +1,18 @@
+### HTTP
+
+#### 1.使用
+
+接口请求，向云服务器发送上传图片请求，需要构造请求头
+
+
+
+#### 参考资料
+
+<https://www.cnblogs.com/kingszelda/p/8988505.html>
+
+<https://www.jianshu.com/p/a768201c1997>
+
+<https://www.jianshu.com/p/cfefdc4e062e>
+
+
+
---
+++ java/src/main/java/net/ibytes/study/http/HttpClientPoolUtil.java
@@ -0,0 +1,48 @@
+package net.ibytes.study.http;
+
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+
+import java.util.concurrent.ScheduledExecutorService;
+
+/**
+ * @author dingfurong
+ * @date 2019/7/19
+ * @description httpclient连接池
+ */
+public class HttpClientPoolUtil {
+
+    /**
+     * 设置连接建立的超时时间为10s
+     */
+    private static final int CONNECT_TIMEOUT = 10;
+    /**
+     *
+     */
+    private static final int SOCKET_TIMEOUT = 10;
+    /**
+     * 最大连接数
+     */
+    private static final int MAX_CONN = 12;
+    private static final int Max_PRE_ROUTE = 12;
+    private static final int MAX_ROUTE = 12;
+
+    /**
+     *  发送请求的客户端单例
+     */
+    private static CloseableHttpClient httpClient;
+    /**
+     * 连接池管理类
+     */
+    private static PoolingHttpClientConnectionManager manager;
+
+
+    private static ScheduledExecutorService monitorExecutor;
+
+    /**
+     *  相当于线程锁,用于线程安全
+     */
+    private final static Object syncLock = new Object();
+
+
+}
